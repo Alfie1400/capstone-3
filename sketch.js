1de8777daba7222c59d988934d20aec3fc7f5fe7
Alfie1400 <116584540+Alfie1400@users.noreply.github.com>
2023-03-15 20:17:45 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+var ball
+var ground1
+var ground2
+
+function setup() {
+	createCanvas(1000, 500);
+
+	var ball_options = {
+		restitution:0.9,
+		friction:0,
+		density:0.1
+	}
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+	ball = Bodies.circle(250,50,20,ball_options)
+	World.add(world,ball)
+	ground  = new Ground(500,480,1000,10)
+	ground1  = new Ground(700,150,10,40)
+	ground2  = new Ground(800,150,10,40)
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+
+  ellipse(ball.position.x,ball.position.y,20)
+	ground.display()
+	ground1.display()
+	ground2.display()
+
+  drawSprites();
+ 
+}
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(ball,ball.position,{x:0.9,y:-1})
+	}
+}
+
+
+
